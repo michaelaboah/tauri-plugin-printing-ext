5c63a2a2620e055e088450e7bd0b0a314350cf99
michaelaboah <michaelaboah@gmail.com>
2023-09-28 09:30:51 -0400

    "removed jsPDF"

--- webview-dist/index.d.ts
+++ webview-dist/index.d.ts
@@ -1,1 +1,13 @@
-export declare function printDialog(htmlTarget: HTMLElement): Promise<void>;
+/**
+ * Opens a print dialog for a PDF with base64 data prodivded.
+ * This function is specific to Darwin or macOS platforms.
+ *
+ * Note: *jsPDF to transform html into base64 pdf*
+ *
+ * @param {string} base64 - transformed pdf-data
+ * @returns {void} If the platform is not Darwin or macOS, logs an error and returns.
+ *
+ * @example
+ * printDialog(document.getElementById('printableArea'));
+ */
+export declare function printDialog(base64: string): Promise<void>;
--- webview-src/index.ts
+++ webview-src/index.ts
@@ -1,26 +1,23 @@
 import { invoke } from '@tauri-apps/api/tauri'
-import { jsPDF } from "jspdf"
-
-export async function printDialog(htmlTarget: HTMLElement) {
+/**
+ * Opens a print dialog for a PDF with base64 data prodivded.
+ * This function is specific to Darwin or macOS platforms.
+ * 
+ * Note: *jsPDF to transform html into base64 pdf* 
+ *
+ * @param {string} base64 - transformed pdf-data
+ * @returns {void} If the platform is not Darwin or macOS, logs an error and returns.
+ *
+ * @example
+ * printDialog(document.getElementById('printableArea'));
+ */
+export async function printDialog(base64: string) {
  
-  console.log("Attempting print")
-  let target = structuredClone(htmlTarget);
+  if (!navigator.userAgent.includes("Mac")) {
+    return console.error("Darwin or macOs specific operation")
+  }
 
-  // if (!navigator.userAgent.includes("Mac")) {
-  //   return console.error("Darwin or macOs specific operation")
-  // }
-
-  const doc = new jsPDF({orientation: "p", unit: "px", format: "letter", hotfixes: ["px_scaling"] });
-
-    doc.html(target, {
-
-      callback: (doc) => {
-        let base64 = doc.output("datauristring").split(",")[1]
-        invoke('plugin:printing-ext|print_dialog', { base64 })
-      },
-
-    })
-
+  invoke('plugin:printing-ext|print_dialog', { base64 })
 }
 
 
